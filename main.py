21d87653e1cbaf07f152b3c5e1de319f82f6bedf
pvelgersdyk <pvelgersdyk@westmont.edu>
2025-05-31 12:39:01 -0700

    Update main.py

--- main.py
+++ main.py
@@ -101,4 +101,5 @@ def serve_openapi():
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
+
